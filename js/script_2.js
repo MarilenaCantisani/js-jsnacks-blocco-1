95bb5fc9669a81d7e5f2d454cfc88aa1e8d5ae8e
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-07 15:57:21 +0200

    + Added Snack 2

--- js/script_2.js
+++ js/script_2.js
@@ -1,2 +1,23 @@
 /* SNACK 2: Chiedi un numero all'utente, se è pari stampa il numero,
 se è dispari stampa il numero successivo. */
+
+//? #1: Chiedere all'utente un numero; 
+//? #2: Verificare se il numero è pari o dispari;
+//? #3: Se è pari -> stampare il numero scelto;
+//? #4: Altrimenti, se è dispari -> stampare il numero successivo.
+
+
+//TODO: #1: Chiedere all'utente un numero; 
+do {
+    var userNumbers = parseInt(prompt("Dimmi un numero"));
+} while (isNaN(userNumbers))
+
+console.log("Il numero inserito dall'utente è: ", userNumbers);
+
+//TODO: #2 / #3 / #4 
+if (userNumbers % 2 === 0) {
+    console.log("Numero pari: ", userNumbers);
+} else {
+    console.log("Numero dispari (aumentato di uno): ", userNumbers + 1);
+}
+
